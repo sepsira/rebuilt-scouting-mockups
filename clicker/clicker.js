99acf0ad653ecfc49f693fd731278d88b243b91b
Ela S <u23.277@gmail.com>
2026-01-12 11:54:48 -0800

    Add click event to increment count on button press

--- clicker/clicker.js
+++ clicker/clicker.js
@@ -1,1 +1,10 @@
-
+(function() {
+    const button = document.getElementById("clicker");
+    const total = document.getElementById("count");
+    let count = parseInt(total.innerHTML);
+    
+    button.addEventListener("click", function() {
+        total.innerHTML = count + 1;
+        count += 1;
+    });
+}) ();
