ea4d74829e80303b98925e4fa23460065804f91a
Ela S <u23.277@gmail.com>
2026-01-12 12:09:13 -0800

    Implement button click handlers for count increments
    Added event listeners to buttons for incrementing count.

--- increments/increment.js
+++ increments/increment.js
@@ -1,1 +1,22 @@
+(function() {
+    const button1 = document.getElementById("clicker-1");
+    const button5 = document.getElementById("clicker-5");
+    const button10 = document.getElementById("clicker-10");
+    const total = document.getElementById("count");
+    let count = parseInt(total.innerHTML);
+    
+    button1.addEventListener("click", function() {
+        total.innerHTML = count + 1;
+        count += 1;
+    });
+    
+    button5.addEventListener("click", function() {
+        total.innerHTML = count + 5;
+        count += 5;
+    });
 
+    button10.addEventListener("click", function() {
+        total.innerHTML = count + 10;
+        count += 10;
+    });
+}) ();
