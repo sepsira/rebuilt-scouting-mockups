0be5521ff87ca4cf264bd992412f7208b48664e4
Ela S <u23.277@gmail.com>
2026-01-13 19:09:19 -0800

    Implement keyboard event to increment count
    Added keyboard support to increment count with 'z' key.

--- clicker/clicker.js
+++ clicker/clicker.js
@@ -7,4 +7,12 @@
         total.innerHTML = count + 1;
         count += 1;
     });
+
+    document.addEventListener("keydown", function(t) {
+        if (t.key === "z") {
+            event.preventDefault(); 
+            total.innerHTML = count + 1;
+            count += 1;
+        }
+    });
 }) ();
